1f8898a576f2ba50570e88da8be0d31f687178c4
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-01-19 11:21:33 +0500

    Use fast-apply

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var apply = require('fast-apply');
+
 module.exports = function read(res, options, cb) {
 
 	var _args = arguments;
@@ -44,6 +46,6 @@ module.exports = function read(res, options, cb) {
 			args.push(_args[i]);
 		}
 
-		cb.apply(null, args);
+		apply(cb, null, args);
 	});
 };
